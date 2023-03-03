ee69e68626b2d8ce01589d81eeee05440cb9c24f
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2023-03-03 20:20:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-function markListLabelsByLabelName(targetLabel='Milestone 1', listIndex=0, speed=2000) {
+function markListLabelsByLabelName(targetLabel = 'Milestone 1', listIndex = 0, speed = 2000) {
     /* for only not labeled cards*/
-    if (speed<2000){
-       console.log("min speed is 2000");
+    if (speed < 2000) {
+        console.log("min speed is 2000");
         return false;
     }
-    
+
     let timeouts = 0;
     let automationI = 0;
     let targetList = null;
-    if (listIndex < $('.js-list.list-wrapper').length && $('.js-list.list-wrapper').eq(listIndex).length){
+    let foundAtLeastCard = false;
+    if (listIndex < $('.js-list.list-wrapper').length && $('.js-list.list-wrapper').eq(listIndex).length) {
         targetList = $('.js-list.list-wrapper').eq(listIndex);
     } else {
-      console.log(`you do not have enogh lists please set valid listIndex starting from 0 to first list, current Lists: ${$('.js-list.list-wrapper').length}`);
+        console.log(`you do not have enogh lists please set valid listIndex starting from 0 to first list, current Lists: ${$('.js-list.list-wrapper').length}`);
         return false;
     }
-    
-    console.log("this cards in the list not have a label, script will automate them.");
+
     const allLabels = $(targetList).find(".list-card.js-member-droppable.ui-droppable.ui-sortable-handle");
     $(allLabels).each((i, elm) => {
         if (!$(elm).find(".js-list-card-front-labels-container .ef93UiHgHz1OFw").length) {
+            if (foundAtLeastCard == false) {
+                console.log("this cards in the list not have a label, script will automate them.");
+                foundAtLeastCard = true;
+            }
+
             console.log(`${i} not have a label`);
             if ($(elm).find(".list-card-operation").length) {
                 timeouts += speed;
@@ -29,7 +34,7 @@ function markListLabelsByLabelName(targetLabel='Milestone 1', listIndex=0, speed
                         $(targetElm).click();
                         const editBtn = $(".quick-card-editor a.quick-card-editor-buttons-item.js-edit-labels");
                         if ($(editBtn).length) {
-                            $(editBtn).click();                                               
+                            $(editBtn).click();
                             setTimeout(() => {
                                 const currentLabels = $("section[data-testid='labels-popover-labels-screen'] ul li");
                                 $(currentLabels).each((ei, labelLi) => {
@@ -37,38 +42,38 @@ function markListLabelsByLabelName(targetLabel='Milestone 1', listIndex=0, speed
                                     if ($(targetLabelELm).length &&
                                         ($(targetLabelELm).text().trim() == targetLabel.trim())) {
                                         console.log(`found label for card index: ${i}`);
-                    const liParent = $(targetLabelELm).closest("li");
-                        if ($(liParent).length && !$(liParent).find("span.P98k_n1VEYvMVQ").length && $(liParent).find("span svg").length){
-                            const checkBoxSpan = $(liParent).find("span.CpyGgjAzUkQDno");
-                            $(checkBoxSpan).click();
-                            if ($("section[data-testid='labels-popover-labels-screen'] button[aria-label='Close popover']").length){
-                                
-                              $("section[data-testid='labels-popover-labels-screen'] button[aria-label='Close popover']").click();
-                                console.log("hi");
-                            }
-                        }
-                                                                
+                                        const liParent = $(targetLabelELm).closest("li");
+                                        if ($(liParent).length && !$(liParent).find("span.P98k_n1VEYvMVQ").length && $(liParent).find("span svg").length) {
+                                            const checkBoxSpan = $(liParent).find("span.CpyGgjAzUkQDno");
+                                            $(checkBoxSpan).click();
+                                            if ($("section[data-testid='labels-popover-labels-screen'] button[aria-label='Close popover']").length) {
+
+                                                $("section[data-testid='labels-popover-labels-screen'] button[aria-label='Close popover']").click();
+                                            }
+                                        }
+
                                     }
 
                                 });
-           
+
                             }, 1000);
 
                         }
                     }
                 }, timeouts);
-                
+
 
             }
         }
     });
-    setTimeout(()=>{
-        if ($(".quick-card-editor-card input[type='submit']").length){
-$(".quick-card-editor-card input[type='submit']").click();
+    setTimeout(() => {
+
+        if ($(".quick-card-editor-card input[type='submit']").length) {
+            $(".quick-card-editor-card input[type='submit']").click();
+        }
+        if (foundAtLeastCard != true) {
+            console.log('no cards proccessed');
         }
         console.log('completed automation proccess');
-    }, timeouts+1200);
+    }, timeouts + 1200);
 }
-
-/* call example : markListLabelsByLabelName('Milestone 1', 1); // this calls mean add label Milestone 1 for all cards not have label in list 2, 
-listIndex start from 0 so 0 is first list and 1 is second list, you can control speed, and labels*/
